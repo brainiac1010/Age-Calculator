6cd04de347887ffae1d8c41d762408ec88e437b2
brainiac1010 <armanmahim00@gmail.com>
2024-04-17 18:41:14 +0600

    comment remove from js script

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function CalculateAge() {
     let m1 = birthDate.getMonth() + 1;
     let y1 = birthDate.getFullYear();
     let today = new Date();
-    let d2 = today.getDate(); // Fixed: should be today's date, not birthDate
-    let m2 = today.getMonth() + 1; // Fixed: should be today's month, not birthDate
-    let y2 = today.getFullYear(); // Fixed: should be today's year, not birthDate
+    let d2 = today.getDate(); 
+    let m2 = today.getMonth() + 1; 
+    let y2 = today.getFullYear(); 
 
     let d3, m3, y3;
     y3 = y2 - y1;
@@ -21,8 +21,8 @@ function CalculateAge() {
         y3--;
         m3 = 12 + m2 - m1;
     }
-    if (d2 >= d1) { // Fixed: comparing with today's date
-        d3 = d2 - d1; // Fixed: today's date - birthDate's date
+    if (d2 >= d1) { 
+        d3 = d2 - d1; 
     } else {
         m3--;
         d3 = getDayesInMonth(y1, m1) + d2 - d1;
